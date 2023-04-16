385c5243b2b69d183199ef2bfd9c516946c5eead
dordieux <dordieux@gmail.com>
2023-04-16 20:55:21 +0900

    add commend message

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,7 +1,11 @@
 import { Enemy, Player } from "../objects";
 
 export class GameScene extends Phaser.Scene {
-  constructor(private player: Player, private enemy: Enemy) {
+  constructor(
+    private bitmapText: Phaser.GameObjects.BitmapText,
+    private player: Player,
+    private enemy: Enemy
+  ) {
     super({
       key: "GameScene",
     });
@@ -14,6 +18,14 @@ export class GameScene extends Phaser.Scene {
   }
 
   create(): void {
+    this.bitmapText = this.add.bitmapText(
+      16,
+      16,
+      "font",
+      "ATTACK : A\n" + "BLOCK  : D\n" + "JUMP   : SPACE",
+      16
+    );
+
     this.player = new Player({
       scene: this,
       x: 150,
